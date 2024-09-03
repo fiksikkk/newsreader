24cd6e120a9f34999ac44e778b8022726a9de745
Yakov Varenik <yakov@railsmuffin.com>
2024-09-03 19:00:47 +0300

    upd testing-library

--- .eslintrc.js
+++ .eslintrc.js
@@ -24,4 +24,11 @@ module.exports = {
       },
     ],
   },
+  overrides: [
+    {
+      // Test files only
+      files: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)'],
+      extends: ['plugin:testing-library/react'],
+    },
+  ],
 };
