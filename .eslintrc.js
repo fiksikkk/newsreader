6ebc0c335a0d4544ca8d7dffee3f09ef2df05bda
Yakov Varenik <yakov@railsmuffin.com>
2024-09-03 12:32:03 +0300

    add rules to eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,27 @@
 module.exports = {
   root: true,
+  parser: '@typescript-eslint/parser',
   extends: '@react-native',
+  plugins: ['import'],
+  rules: {
+    'import/order': [
+      2,
+      {
+        groups: ['builtin', 'external', 'internal', 'sibling'],
+        pathGroups: [
+          {
+            pattern: 'react',
+            group: 'builtin',
+            position: 'before',
+          },
+        ],
+        pathGroupsExcludedImportTypes: ['react'],
+        'newlines-between': 'always',
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true,
+        },
+      },
+    ],
+  },
 };
